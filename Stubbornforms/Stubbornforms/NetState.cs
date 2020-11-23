2c0fa7fac963d6e4b34169f18f87e0f22607e979
Kövér Márton <koverm0414@gmail.com>
2020-11-23 16:06:53 +0100

    Add files via upload

---
+++ Stubbornforms/Stubbornforms/NetState.cs
@@ -0,0 +1,101 @@
+﻿using PetriNetBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stubbornforms
+{
+    public class NetState
+    {
+        private static int counter = 0;
+
+        public int[] States 
+        {
+            get;
+            private set;
+        }
+
+        public int Id
+        {
+            get;
+            private set;
+        }
+        public List<NetState> Neighbours
+        {get;set;}
+
+        static public void initCounter() {
+            counter = 0;
+        }
+
+        public NetState(List<Place> places)
+        {
+            Id = counter++;
+            Neighbours = new List<NetState>();
+
+            States = new int[places.Count];
+
+            for (int i = 0; i < States.Length; i++)
+            {
+                States[i] = places[i].TokenCount;
+            }
+        }
+        public NetState(NetState netState, NetTransition trans)
+        {
+            Id = counter++;
+            Neighbours = new List<NetState>();
+
+            if (netState.fireable(trans))
+            {
+                States = new int[netState.States.Length];
+                for (int i = 0; i < States.Length; i++)
+                {
+                    States[i] = netState.States[i] - trans.inEdges[i] + trans.outEdges[i];
+                }
+            }
+            else {
+                States = new int[netState.States.Length];
+                for (int i = 0; i < States.Length; i++)
+                {
+                    States[i] = 0;
+                }
+            }
+        }
+
+        public bool fireable(NetTransition nt) {
+            if (nt.inEdges.Length != States.Length)
+                return false;
+
+            for (int i = 0; i < States.Length; i++)
+            {
+                if ((States[i] - nt.inEdges[i]) < 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public void fire(NetTransition nt) {
+            if (fireable(nt)) {
+                for (int i = 0; i < States.Length; i++)
+                {
+                    States[i] = States[i] - nt.inEdges[i] + nt.outEdges[i]; 
+                }
+            }
+        }
+
+        public string toString() {
+            string message = "";
+
+            for (int i = 0; i < States.Length-1; i++)
+            {
+                message += States[i];
+            }
+            message += States[States.Length - 1];
+            
+            return message;
+        }
+        
+    }
+}
---
+++ Stubbornforms/Stubbornforms/NetTransition.cs
@@ -0,0 +1,76 @@
+﻿using PetriNetBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stubbornforms
+{
+
+    public class NetTransition
+    {
+        public int[] inEdges { get; private set; }
+        public int[] outEdges { get; private set; }
+
+        public NetTransition(List<Place> places, Transition transition)
+        {
+            inEdges = new int[places.Count];
+            outEdges = new int[places.Count];
+
+            for (int i = 0; i < inEdges.Length; i++)
+            {
+                inEdges[i] = 0;
+                outEdges[i] = 0;
+            }
+
+            List<Edge> inputEdges = transition.InputEdges;
+
+            foreach (var edge in inputEdges)
+            {
+                int i = places.IndexOf((Place)edge.Source);
+                if (i > -1) {
+                    inEdges[i] = edge.Weight;
+                }
+                
+            }
+
+            List<Edge> outputEdges = transition.OutputEdges;
+
+            foreach (var edge in outputEdges)
+            {
+                int i = places.IndexOf((Place)edge.Target);
+                if (i > -1)
+                {
+                    outEdges[i] = edge.Weight;
+                }
+
+            }
+
+        }
+
+        public string InEdgesToString() {
+            string message = "";
+
+            for (int i = 0; i < inEdges.Length - 1; i++)
+            {
+                message += inEdges[i] + ",";
+            }
+            message += inEdges[inEdges.Length - 1];
+
+            return message;
+        }
+        public string OutEdgesToString()
+        {
+            string message = "";
+
+            for (int i = 0; i < outEdges.Length - 1; i++)
+            {
+                message += outEdges[i] + ",";
+            }
+            message += outEdges[outEdges.Length - 1];
+
+            return message;
+        }
+    }
+}
